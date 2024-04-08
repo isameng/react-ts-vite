731077993f10e3a155cad4a1391c43dc18091f9a
ameng <>
2024-04-08 17:26:00 +0800

    init: 初始化

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react-refresh'],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+  }
+}
